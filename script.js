adeef3fdb800e615cf6964c8e3777a8af4e9fe65
Soham Roy <sohammusic38@gmail.com>
2022-08-29 09:16:30 +0530

    removed second button glitch

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let mainBtn = document.querySelector(".main_btn");
 let pass1 = document.querySelector("#pass_1");
 let pass2 = document.querySelector("#pass_2");
-let passwords = document.querySelector(".result");
 let generateClicked = false;
 
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
@@ -15,11 +14,20 @@ mainBtn.addEventListener("click", function() {
     pass2.textContent = p2;
 } );
 
-passwords.addEventListener("click", function() {
+pass1.addEventListener("click", function() {
     if ( !generateClicked ) {
         return;
     }
-    let text = passwords.textContent;
+    let text = pass1.textContent;
+    navigator.clipboard.writeText(text);
+    showSnackbar();
+});
+
+pass2.addEventListener("click", function() {
+    if ( !generateClicked ) {
+        return;
+    }
+    let text = pass2.textContent;
     navigator.clipboard.writeText(text);
     showSnackbar();
 });
